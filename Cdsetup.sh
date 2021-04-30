ddd4cff945527d97aac6b2d1c57804ef030a017b
Yiğit Cemal Öztürk <69117901+CadmiumC4@users.noreply.github.com>
2021-04-30 13:27:56 +0300

    Update Cdsetup.sh

--- Cdsetup.sh
+++ Cdsetup.sh
@@ -1,6 +1,39 @@
 sudo apt-get install git
+## Update all packages in your distro
+
+sudo apt update
+
+## Gedit
+
 sudo apt install gedit -y
-sudo apt install nautilus -y
+
+## GIMP
+
 sudo apt install gimp -y
+
+## Nautilus
+
+sudo apt install nautilus -y
+
+## VLC
+
+sudo apt install vlc -y
+
+## X11 apps
+
+sudo apt install x11-apps -y
+
+## Google Chrome
+
+## cd /tmp
+
+####sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+sudo dpkg -i google-chrome-stable_current_amd64.deb 
+
+sudo apt install --fix-broken -y
+
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+#####
 curl https://sh.rustup.rs -sSf | sh
 
